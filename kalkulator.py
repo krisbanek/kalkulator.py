0fac80b6a9fa29b26ed6435ba9e483a3e3eca7a4
Krzysztof Banek <krisbanek@gmail.com>
2021-12-01 10:06:55 +0100

    Add logging inside functions & exit if wrong choise done

--- kalkulator.py
+++ kalkulator.py
@@ -3,15 +3,19 @@ import logging
 logging.basicConfig(level=logging.DEBUG, format='%(message)s')
 
 def add(first_number, second_number):
+    logging.info(f"Dodaję: {first_number} i {second_number}")
     return first_number + second_number
 
 def subtract(first_number, second_number):
+    logging.info(f"Odejmuję: {second_number} od {first_number}")
     return first_number - second_number
 
 def multiply(first_number, second_number):
+    logging.info(f"Mnożę: {first_number} i {second_number}")
     return first_number * second_number
 
 def divide(first_number, second_number):
+    logging.info(f"Dzielę: {first_number} przez {second_number}")
     return first_number / second_number
 
 calc_dict = {
@@ -27,14 +31,12 @@ def calc(sign, first_number, second_number):
 
 if __name__ == "__main__":
     sign = int(input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: "))
+    if sign >= 1 and sign <=4:
+        pass
+    else:
+        logging.info("Nie wybrałeś liczby z zakresu 1 - 4. Podaj poprawną liczbę odpowiadającą działaniu: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie.")
+        exit(1)
     first_number = float(input("Podaj składnik 1: "))
     second_number = float(input("Podaj składnik 2: "))
-    calc_info = {
-    "1": f"Dodaję {first_number} i {second_number}",
-    "2": f"Odejmuję {second_number} od {first_number}",
-    "3": f"Mnożę {first_number} i {second_number}",
-    "4": f"Dzielę {first_number} przez {second_number}"
-    }
-    logging.info(calc_info[f"{sign}"])
     calc_result = calc(sign, first_number, second_number)
     print(f"Wynik to: {calc_result}")
